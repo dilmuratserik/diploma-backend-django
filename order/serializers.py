571ac3f8a695f7b768c5bd8fa0a1bfc8eae41ec7
Akniet <akniet1805@gmail.com>
2021-10-26 17:07:03 +0600

    first

--- order/serializers.py
+++ order/serializers.py
@@ -3,8 +3,14 @@ from .models import *
 from users.serializers import contgentSer
 from product.serializers import productSer2
 
+
+class productser(serializers.Serializer):
+    name = serializers.CharField()
+    code = serializers.CharField()
+    articul = serializers.CharField()
+
 class OrderSer(serializers.ModelSerializer):
-    products = productSer2(many=True)
+    products = productSer(many=True)
     counterparty = contgentSer()
     class Meta:
         model = Order
